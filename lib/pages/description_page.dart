2544038c0730c31543f25d9331dbebd8e3ccef7a
Agung Suprayitno <dev.agungsuprayitno@gmail.com>
2023-06-02 10:03:27 +0700

    Minute 1:45:00

--- lib/pages/description_page.dart
+++ lib/pages/description_page.dart
@@ -2,15 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_init/core/constants.dart';
 import 'package:flutter_init/data/item_class.dart';
 
-class DescriptionPage extends StatelessWidget {
+class DescriptionPage extends StatefulWidget {
   const DescriptionPage({super.key, required this.item});
   final ItemClass item;
 
   @override
+  State<DescriptionPage> createState() => _DescriptionPageState();
+}
+
+class _DescriptionPageState extends State<DescriptionPage> {
+  double fontSizeCustom = 25;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(item.title),
+        title: Text(widget.item.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Hello Snackbar'),
+                  behavior: SnackBarBehavior.floating,
+                ),
+              );
+            },
+            icon: const Icon(Icons.info),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -18,38 +37,54 @@ class DescriptionPage extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                Image.asset(item.imagePath),
+                Image.asset(widget.item.imagePath),
                 const SizedBox(height: kDouble20),
                 Wrap(
                   spacing: kDouble10,
                   children: [
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          fontSizeCustom = 20;
+                        });
+                      },
                       child: const Text('Small Title'),
                     ),
                     OutlinedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          fontSizeCustom = 80;
+                        });
+                      },
                       child: const Text('Medium Title'),
                     ),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          fontSizeCustom = 120;
+                        });
+                      },
                       child: const Text('Large Title'),
                     ),
                     FilledButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        setState(() {
+                          fontSizeCustom = 200;
+                        });
+                      },
                       child: const Text('Huge Title'),
                     )
                   ],
                 ),
                 FittedBox(
                   child: Text(
-                    item.title,
-                    style: const TextStyle(
-                        fontSize: 40, fontWeight: FontWeight.bold),
+                    widget.item.title,
+                    style: TextStyle(
+                        fontSize: fontSizeCustom, fontWeight: FontWeight.bold),
                   ),
                 ),
                 Text(
-                  item.longDescription,
+                  widget.item.longDescription,
                   style: const TextStyle(fontSize: 18),
                   textAlign: TextAlign.justify,
                 ),
